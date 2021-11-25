edb65dfc6de73d4093677b8634224456d29919ed
Yang Yue <johnyangyue@gmail.com>
2021-11-25 16:55:06 -0600

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,30 @@ import cv2
 import gym
 import numpy as np
 
+def plot_scores(x_axis,scores,avg_scores,min_scores,max_scores,epsilon,filename):
+    n= len(scores)
+    running_avg = np.empty(n)
+    for t in range(n):
+        running_avg[t] = np.mean(scores[max(0,t-100):(t+1)])
+
+    f,a = plt.subplots()
+    sns.lineplot(x=x_axis,y=scores,label='Raw',ax=a,color='deepskyblue')
+    sns.lineplot(x=x_axis,y=avg_scores,label='Average',ax=a,linestyle='dashdot',color='darkorange')
+    #sns.lineplot(x=x_axis,y=min_scores,label='Minimum',ax=a,linestyle='dashed',color='dimgray')
+    #sns.lineplot(x=x_axis,y=max_scores,label='Maximum',ax=a,linestyle='dashdot',color='dimgrey')
+    a.set_xlabel('Training Episodes')
+    a.set_ylabel('Scores')
+    plt.legend(bbox_to_anchor=(1.1,1),loc='upper left')
+
+    a_alt = a.twinx()
+    sns.lineplot(x=x_axis,y=epsilon,color='aqua',linestyle='dotted')
+    a_alt.set_ylabel('Epsilons')
+
+    plt.title('Training History')
+    plt.tight_layout()
+    plt.savefig(filename,bbox_inches='tight',dpi=200)
+
+"""
 def plot_reward(episodes, avg_score, min_score, max_score, epsilon, game_name):
   fig, ax1 = plt.subplots()
   if len(episodes) == len(avg_score):
@@ -23,6 +47,7 @@ def plot_reward(episodes, avg_score, min_score, max_score, epsilon, game_name):
     plt.title('Reward on '+game_name)
     fig.tight_layout() 
     plt.show()
+"""
 
 class TimeCapsule():
     def __init__(self, max_mems):
